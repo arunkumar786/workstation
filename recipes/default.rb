092d129a0065c12eb3beb4d39ec3b8e7da36779c
arunkumar786 <techlinux4arun@gmail.com>
2017-10-24 00:33:58 +0530

    workstation default.rb includes setup.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# <include_recipe> syntax will call other recipe in# same or other cookbook. Note that we have recipe # as of now we are calling recipe in the same cookb# ook.
+
+include_recipe 'workstation::setup'
+# include_recipe 'apache::server'
