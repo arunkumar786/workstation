1a0897bbe86ddc1f46a65ec0854c767cdb71a5f7
arunkumar786 <techlinux4arun@gmail.com>
2017-11-01 01:22:54 +0530

    refactored motd file using templates

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,20 +12,29 @@ end
 
 package 'emacs'
 
-file '/etc/motd' do
-  # accessing the node objects to update the
-  # system info instead of hardcoding.
-  # here we use pound curly brace syntax
-  # to extrac the nod info.
-  content "This server is the property of Achutha
-           HOSTNAME: #{node['hostname']}
-           IPADDRESS: #{node['ipaddress']}
-           CPU: #{node['cpu']['0']['mhz']}
-           MEMORY: #{node['memory']['total']}"
-  # default property
+# so here instead using this file 
+# resource we use template resource.
+#file '/etc/motd' do
+#  # accessing the node objects to update the
+#  # system info instead of hardcoding.
+#  # here we use pound curly brace syntax
+#  # to extrac the nod info.
+#  content "This server is the property of Achutha
+#           HOSTNAME: #{node['hostname']}
+#           IPADDRESS: #{node['ipaddress']}
+#           CPU: #{node['cpu']['0']['mhz']}
+#           MEMORY: #{node['memory']['total']}"
+#  # default property
+#  action :create
+#  owner 'root'
+#  group 'root'
+#end
+# here template file path
+# source file is the one where the content
+# is copied.
+template '/etc/motd' do
+  source 'motd.erb'
   action :create
-  owner 'root'
-  group 'root'
 end
 
 service 'ntp' do
